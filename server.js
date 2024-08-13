fbfe11cc0d0a01804b8bd9364985383802f9f529
Abrorkhon <akon.xan@mail.ru>
2024-08-13 18:02:05 +0900

    BRR: BUILDING EXPRESS Web SERVER

---
+++ server.js
@@ -0,0 +1,30 @@
+console.log("WebServerni Boshlash");
+
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1.kirish codes
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //html form
+
+// 2. zsesssion bogliq express
+
+// 3.express uchun bss backend server side videw codes
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4. routing
+app.get("/hello", function (req, res) {
+  res.end("<h1>Hello World by  Arorkon</h1>");
+});
+app.get("/gift", function (req, res) {
+  res.end("<h1>siz sovgalar sahifasidasiz</h1>");
+});
+const server = http.createServer(app);
+
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`Server 3000 Portda Muvafaqiyyatli ishlayapti:${PORT}`);
+});
