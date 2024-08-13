b42438cfce6d31c0fcf8b7722166128c177616d6
Abrorkhon <akon.xan@mail.ru>
2024-08-13 19:13:30 +0900

    feat

--- server.js
+++ server.js
@@ -16,13 +16,21 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4. routing
-app.get("/hello", function (req, res) {
-  res.end("<h1>Hello World by  Arorkon</h1>");
-});
-app.get("/gift", function (req, res) {
-  res.end("<h1>siz sovgalar sahifasidasiz</h1>");
-});
+// app.get("/hello", function (req, res) {
+//   res.end("<h1>Hello World by  Arorkon</h1>");
+// });
+// app.get("/gift", function (req, res) {
+//   res.end("<h1>siz sovgalar sahifasidasiz</h1>");
+// });
 const server = http.createServer(app);
+app.post("/create-item",(req,res)=>{
+console.log(req.body)
+res.json({test:"success"})
+});
+
+app.get('/',function(req,res){
+    res.render("harid")
+})
 
 let PORT = 3000;
 server.listen(PORT, function () {
